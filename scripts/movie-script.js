89fe3871b94638b656cf1911ed27279715e308ba
Matthew Howell <78524878+mattimus11@users.noreply.github.com>
2024-07-29 16:58:43 -0500

    Add files via upload

--- scripts/movie-script.js
+++ scripts/movie-script.js
@@ -1,31 +1,47 @@
 $(document).ready(function () {
   console.log("loaded and ready!!");
 
-//Button Click Issue # 1
+  let movieList = [
+      { title: "Lord of the Rings", rating: 10 },
+      { title: "Cloudy with a Chance of Meatballs", rating: 9.5 },
+      { title: "The Princess Bride", rating: 9.3 },
+      { title: "Ratatouille", rating: 9.1 },
+      { title: "How to Train Your Dragon 2", rating: 9.0 }
+  ];
 
+  // Function to show list
+  function renderMovieList(movies) {
+      $('#movieList').empty();
+      movies.forEach(movie => {
+          $('#movieList').append(`<li class="list-group-item">${movie.title} - Rating: ${movie.rating}/10</li>`);
+      });
+  }
 
-$('#btnAddMovie').click(function(){
-  console.log($(this).text());
-for (i=0;i<movieList.length;i++){
-  $('#movieList').append(`<li class = "darker-li">${movieList[i]}</li>`)
-}
-})
+  // Sort movies
+  function sortAndRenderMovies() {
+      movieList.sort((a, b) => b.rating - a.rating);
+      renderMovieList(movieList);
+  }
 
-$('#btnRemoveMovie').click(function(){
-  console.log($(this).text());
-$('li').last().remove();
+  // Show and sort
+  sortAndRenderMovies();
 
-})
+  // Add Movie
+  $('#btnAddMovie').click(function() {
+      let movieName = prompt("Enter the movie name:");
+      let movieRating = parseFloat(prompt("Enter the movie rating:"));
 
-let movieList = [
-  "Lord the the Rings Trilogy",
-  "Cloudy with a Chance of Meatballs",
-  "Princess Bride",
-  "Ratatouille",
-  "How to Train Your Dragon 2"
-];
+      if (movieName && !isNaN(movieRating) && movieRating >= 0 && movieRating <= 10) {
+          movieList.push({ title: movieName, rating: movieRating });
+          sortAndRenderMovies();
+      } else {
+          alert("Invalid movie name or rating.");
+      }
+  });
 
-console.log(movieList);
-
-
+  // Remove Last Movie
+  $('#btnRemoveMovie').click(function() {
+      movieList.pop();
+      sortAndRenderMovies();
+  });
 });
